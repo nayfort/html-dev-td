f8f422de337bf2383e232df657a35f71b5cf4cba
nayfort <mitrofanovvladislav2004@gmail.com>
2024-09-12 18:23:44 +0300

    interaction btns

--- src/components/Content/components/interaction/Interaction.jsx
+++ src/components/Content/components/interaction/Interaction.jsx
@@ -10,19 +10,19 @@ const Interaction = () => {
                     <img src={File} alt="File" className="interaction--picture"/>
                     <div className='interaction--blockTitle'>Registrazione</div>
                     <div className='interaction--blockDescr'>Compilare tutti i campi del modulo di registrazione sul sito web.</div>
-                    <button className='interaction--button'>INIZIARE</button>
+                    <button className='interaction--button' onClick={() => {console.log('Start!')}}>INIZIARE</button>
                 </div>
                 <div className='interaction--mid-block'>
                     <img src={Hat} alt="Hat" className="interaction--picture"/>
                     <div className='interaction--blockTitle'>Consultazione di esperti</div>
                     <div className='interaction--blockDescr'>Dopo la registrazione, rispondere all'invito del Consulente di supporto di Petrol Energia e seguire tutte le sue istruzioni.</div>
-                    <button className='interaction--button'>INIZIARE</button>
+                    <button className='interaction--button' onClick={() => {console.log('Start!')}}>INIZIARE</button>
                 </div>
                 <div className='interaction--last-block'>
                     <img src={Tree} alt="Tree" className="interaction--picture"/>
                     <div className='interaction--blockTitle'>Deposito</div>
                     <div className='interaction--blockDescr'>Dopo la verifica, è necessario depositare un minimo di 250 euro sul conto dell'azionista.</div>
-                    <button className='interaction--button'>INIZIARE</button>
+                    <button className='interaction--button' onClick={() => {console.log('Start!')}}>INIZIARE</button>
                 </div>
             </div>
         </div>
